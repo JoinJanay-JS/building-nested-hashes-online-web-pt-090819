c8b9fb54d1584a581ab69270b608c40053156dfa
Janay Bailey <tjanay87@gmail.com>
2019-09-24 20:30:08 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,26 +8,23 @@ def second_challenge
       
   },
     :hero => {
-      :name => "Romeo",
-      :age => "15"
+  
       },
       :hero_friends => {}
       },
     :capulet => {
        :patriarch => {
-      :name => "Lord Capulet",
-      :age => "50"
+      
     },
     :matriarch => {
-      :name => "Lady Capulet",
-      :age => "51"
+     
     },
     :heroine => {
-      :name => "Juliet",
-      :age => "15",
-      :status => "alive"
+      
     },
-    :heroine_friends=> {}
+    :heroine_friends=> {
+      
+    }
     }
 
   

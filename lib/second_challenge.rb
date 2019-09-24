a7c7cf33eb63afa853b90a0c21baddce53f6c709
Janay Bailey <tjanay87@gmail.com>
2019-09-24 20:19:32 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      
+      :patriarch => {  
+      :name => "Lord Montague",
+      :age => "53"
+  },
+    :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+  },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
+    }
       #your key/value pairs here!
       }, 
     :capulet => {
